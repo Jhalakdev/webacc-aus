9f2fad363c5023dadbce0cdeefaaafa38cc2ac9c
Jhalakdev <108620433+Jhalakdev@users.noreply.github.com>
2024-02-16 03:38:34 +0530

    Update layout.js

--- src/app/services/app-dev/layout.js
+++ src/app/services/app-dev/layout.js
@@ -1,6 +1,6 @@
 export const metadata = {
-    title: 'App Development - Web Accuracy',
-    description: 'Embark on the Future of App Development with Seasoned Developers in Ranchi! 🚀 | React Native and Flutter | iOS and Android |  Node.js, Python, and Java',
+    title: 'App Development in Sydney',
+    description: 'Embark on the Future of App Development with Seasoned Developers in Sydney! 🚀 | React Native and Flutter | iOS and Android |  Node.js, Python, and Java',
 }
 export default function AppLayout({ children }) {
     return <>
